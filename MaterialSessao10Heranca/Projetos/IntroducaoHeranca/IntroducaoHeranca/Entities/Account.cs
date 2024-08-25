806132336dcd17be5a28cf8a2cba1f2e0c01cfd8
Walther Oliveira <waltherpires@hotmail.com>
2024-08-25 04:58:57 -0300

    Casting

--- MaterialSessao10Heranca/Projetos/IntroducaoHeranca/IntroducaoHeranca/Entities/Account.cs
+++ MaterialSessao10Heranca/Projetos/IntroducaoHeranca/IntroducaoHeranca/Entities/Account.cs
@@ -4,9 +4,9 @@ namespace IntroducaoHeranca.Entities
 {
     internal class Account
     {
-        public int Number { get; set; }
-        public string Holder { get; set; }
-        public double Balance { get; set; }
+        public int Number { get; private set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
 
         public Account() { }
 
