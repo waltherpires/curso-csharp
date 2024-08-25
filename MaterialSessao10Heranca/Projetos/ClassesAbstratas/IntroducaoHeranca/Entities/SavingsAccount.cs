f8605f8d8bc3db921219bc641d9ff4a0f8c45785
Walther Oliveira <waltherpires@hotmail.com>
2024-08-25 17:55:58 -0300

    Classes abstratas

---
+++ MaterialSessao10Heranca/Projetos/ClassesAbstratas/IntroducaoHeranca/Entities/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAbstratas.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+        : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
