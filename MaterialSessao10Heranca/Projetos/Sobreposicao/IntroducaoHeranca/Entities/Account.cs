b9b4c0362885929958a036ccd48920f8c6f68f3d
Walther Oliveira <waltherpires@hotmail.com>
2024-08-25 14:57:22 -0300

    Sobreposição

---
+++ MaterialSessao10Heranca/Projetos/Sobreposicao/IntroducaoHeranca/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sobreposicao.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+}
