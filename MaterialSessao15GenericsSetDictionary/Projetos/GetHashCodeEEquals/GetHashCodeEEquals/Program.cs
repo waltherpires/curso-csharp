fb018714095b46d948d10cab91bf19701bfc7cff
Walther Oliveira <waltherpires@hotmail.com>
2024-09-07 20:37:51 -0300

    GetHashCode e Equals

---
+++ MaterialSessao15GenericsSetDictionary/Projetos/GetHashCodeEEquals/GetHashCodeEEquals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using GetHashCodeEEquals.Entities;
+
+namespace GetHashCodeEEquals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b); // Compara a referencia do ponteiro de objeto
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
