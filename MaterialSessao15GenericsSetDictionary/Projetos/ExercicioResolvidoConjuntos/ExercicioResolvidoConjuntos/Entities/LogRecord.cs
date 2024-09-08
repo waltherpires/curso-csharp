eececb21c6df47a0c79b54f6ddc8c400a7d28bec
Walther Oliveira <waltherpires@hotmail.com>
2024-09-07 23:13:30 -0300

    Exercicio resolvido sobre conjuntos

---
+++ MaterialSessao15GenericsSetDictionary/Projetos/ExercicioResolvidoConjuntos/ExercicioResolvidoConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvidoConjuntos.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord)) return false;
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
