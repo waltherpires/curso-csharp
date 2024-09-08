ddc2ad44ecbec31667a5b6e7ef35e819144be073
Walther Oliveira <waltherpires@hotmail.com>
2024-09-07 22:25:17 -0300

    SortedSet exemplos

--- MaterialSessao15GenericsSetDictionary/Projetos/DemoConjunto/DemoConjunto/Program.cs
+++ MaterialSessao15GenericsSetDictionary/Projetos/DemoConjunto/DemoConjunto/Program.cs
@@ -7,18 +7,31 @@ namespace DemoConjunto
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            //union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
 
-            Console.WriteLine(set.Contains("Notebook"));
+            // intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
 
-            foreach (string p in set)
+
+            // difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(T obj in collection)
             {
-                Console.WriteLine(p);
+                Console.Write(obj + " ");
             }
+            Console.WriteLine();
         }
     }
 }
