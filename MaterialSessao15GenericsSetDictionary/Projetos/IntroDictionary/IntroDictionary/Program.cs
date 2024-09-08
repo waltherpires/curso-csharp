85bd558c1ec135550075d636d135e752698bbb37
Walther Oliveira <waltherpires@hotmail.com>
2024-09-08 00:55:00 -0300

    Introducao Dictionary

---
+++ MaterialSessao15GenericsSetDictionary/Projetos/IntroDictionary/IntroDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "999931231";
+            cookies["phone"] = "938129381";
+
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("there is no 'emai' key! ");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES: ");
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
