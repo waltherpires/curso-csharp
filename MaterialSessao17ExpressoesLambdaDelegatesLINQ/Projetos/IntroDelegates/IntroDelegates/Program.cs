bf1a33f871b58d366d1512ba10c5803c816333cf
Walther Oliveira <waltherpires@hotmail.com>
2024-09-09 01:24:02 -0300

    Introducao a delegates

---
+++ MaterialSessao17ExpressoesLambdaDelegatesLINQ/Projetos/IntroDelegates/IntroDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using Course.Services;
+
+namespace IntroDelegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op2 = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+            result = op2(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
