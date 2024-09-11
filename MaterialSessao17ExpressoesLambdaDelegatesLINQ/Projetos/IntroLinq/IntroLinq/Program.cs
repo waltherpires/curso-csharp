4da1aadb03bb98d0c497097284dd75bc7d112421
Walther Oliveira <waltherpires@hotmail.com>
2024-09-11 02:00:36 -0300

    Intro LINQ

---
+++ MaterialSessao17ExpressoesLambdaDelegatesLINQ/Projetos/IntroLinq/IntroLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace IntroLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the query
+            foreach (int x in result)
+            {
+               Console.WriteLine(x);
+            }
+        }
+    }
+}
